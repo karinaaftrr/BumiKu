81151ea682bcc7e4033dca3ef37a0196d79a7946
karinaaftrr <karinafitriamalia05@gmail.com>
2026-06-10 04:42:48 +0700

    Delete app/src/main/java/model directory

--- app/src/main/java/model/Feature.kt
+++
@@ -1,8 +0,0 @@
-package com.example.bumiku.model
-import androidx.compose.ui.graphics.vector.ImageVector
-
-data class Feature(
-    val name: String,
-    val route: String,
-    val icon: ImageVector
-)
