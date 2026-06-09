9fea31a024b19e66fc338f42bc42153b68dcaf33
karinaaftrr <karinafitriamalia05@gmail.com>
2026-06-10 04:32:36 +0700

    Add Color.kt with theme color definitions

---
+++ app/src/main/java/com/example/bumiku/ui/theme/Color.kt
@@ -0,0 +1,17 @@
+package com.example.bumiku.ui.theme
+import androidx.compose.ui.graphics.Color
+
+val NavyMain = Color(0xFF133690)
+val LightBlue = Color(0xFF759EF6)
+val GreenDeep = Color(0xFF105952)
+val GoldYellow = Color(0xFFFFB619)
+val BlackSolid = Color(0xFF000000)
+val RedWarning = Color(0xFFFF0B0B)
+
+val PrimaryDark = NavyMain
+val SecondaryDark = LightBlue
+val TertiaryDark = GreenDeep
+
+val PrimaryLight = LightBlue
+val SecondaryLight = NavyMain
+val TertiaryLight = GoldYellow
